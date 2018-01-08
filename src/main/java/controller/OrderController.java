77ecf8770d8dbf8b5ad9386f8e28a7b6be90eeae
disaquasadi <35128826+disaquasadi@users.noreply.github.com>
2018-01-08 22:36:34 +0700

    Update OrderController.java

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,9 +1,11 @@
 package controller;
 
 import model.Applicant;
+import model.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import service.ApplicantService;
+import service.ApplicationService;
 
 import java.util.List;
 
@@ -16,45 +18,50 @@ public class OrderController {
     @Autowired
     private ApplicantService applicantService;
 
+    @Autowired
+    private ApplicationService applicationService;
+
+    //CREATE Visa == Applicant and Applications
     @RequestMapping(path = "/applicants", method = RequestMethod.POST)
     public int addApplicant(@RequestBody Applicant applicant){
         int id = applicantService.addApplicant(applicant);
         return id;
     }
 
+    //READ Visa by name == Get by keyword
     @RequestMapping(path = "/applicants/find", method = RequestMethod.GET)
     public List<Applicant> findApplicants(@RequestParam(required = true, name = "query") String keyword){
         return applicantService.searchApplicants(keyword);
     }
 
-    @RequestMapping(path = "/applicants/{id}", method = RequestMethod.PUT)
-    public Applicant updateApplicant(@RequestBody Applicant applicant,@PathVariable int id){
-        return applicantService.updateApplicant(applicant);
-    }
-
+    //READ All Visa == Get all Visa
     @RequestMapping(path = "/applicants", method = RequestMethod.GET)
     public List<Applicant> getApplicants(){
         return applicantService.getAllApplicants();
     }
 
-    //Get By ID
+    //READ Visa == Get Visa by ID
     @RequestMapping(path = "/applicants/{id}", method = RequestMethod.GET)
     public Applicant getApplicant(@PathVariable int id){
         return applicantService.getApplicant(id);
     }
 
-    //Delete
+    //READ Visa Applications == Get Applications by ID
+    @RequestMapping(path = "/applications/{id}", method = RequestMethod.GET)
+    public Application getApplication(@PathVariable int id){
+        return applicationService.getApplication(id);
+    }
+
+    //UPDATE Visa == Applicant and Applications
+    @RequestMapping(path = "/applicants/{id}", method = RequestMethod.PUT)
+    public Applicant updateApplicant(@RequestBody Applicant applicant,@PathVariable int id){
+        return applicantService.updateApplicant(applicant);
+    }
+
+    //DELETE Visa == Delete Visa by ID
     @RequestMapping(path = "/applicants/{id}", method = RequestMethod.DELETE)
     public void deleteApplicant(@PathVariable int id){
         Applicant applicant = applicantService.getApplicant(id);
         applicantService.deleteApplicant(applicant);
     }
-
-    //Find By Keyword
-
-    @RequestMapping(path = "applicants/readByKeyword/{keyword}", method = RequestMethod.GET)
-    public List<Applicant> getApplicantByKeyword(@PathVariable String keyword){
-        return applicantService.getApplicantbyKeyword(keyword);
-    }
-
 }
